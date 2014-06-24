247785e86d97eebb56f5bcc1e7162fbb00c5d7dd
Laza Bogdan <laza.bogdan@macro-net.ro>
2014-06-24 23:58:21 +0300

    updated verification for Mosaicpro\WpCore\PluginActivated class;

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $libraries = [
 // Plugin initialization
 add_action('plugins_loaded', function() use ($libraries)
 {
-    if (!class_exists('Mosaicpro\\HtmlGenerators\\Core\\IoC') || !class_exists('Mosaicpro\\WpCore\\Plugin'))
+    if (!class_exists('Mosaicpro\\HtmlGenerators\\Core\\IoC') || !class_exists('Mosaicpro\\WpCore\\PluginActivated'))
         return;
 
     // Get the Container from IoC
